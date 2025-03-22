3b2b3036bc84a3f468641eef43925e6042c8897e
VALLABH S D <145659908+vallabhsd@users.noreply.github.com>
2025-03-22 23:33:04 +0530

    Create sample1.sh

---
+++ sample1.sh
@@ -0,0 +1,54 @@
+======================================
+[*] Checking HTTP response...
+✅ HTTP Status: 301
+[*] Checking SSL certificate...
+issuer=C=US, O=Google Trust Services, CN=WR2
+subject=CN=*.google.com
+notBefore=Jan 20 08:36:04 2025 GMT
+notAfter=Apr 14 08:36:03 2025 GMT
+[*] Scanning open ports...
+[*] Checking HTTP security headers...
+[*] Checking for Web Application Firewall (WAF)...
+✅ No obvious WAF detected.
+[*] Performing WHOIS lookup...
+   Registrar WHOIS Server: whois.markmonitor.com
+   Registrar URL: http://www.markmonitor.com
+   Updated Date: 2019-09-09T15:39:04Z
+   Creation Date: 1997-09-15T04:00:00Z
+   Registrar: MarkMonitor Inc.
+   Registrar IANA ID: 292
+   Registrar Abuse Contact Email: abusecomplaints@markmonitor.com
+   Registrar Abuse Contact Phone: +1.2086851750
+Registrars.
+[*] Fetching DNS records...
+;; communications error to 192.168.149.2#53: host unreachable
+;; communications error to 192.168.149.2#53: host unreachable
+;; communications error to 192.168.149.2#53: host unreachable
+142.250.206.174
+;; communications error to 192.168.149.2#53: host unreachable
+;; communications error to 192.168.149.2#53: host unreachable
+;; communications error to 192.168.149.2#53: host unreachable
+10 smtp.google.com.
+;; communications error to 192.168.149.2#53: host unreachable
+;; communications error to 192.168.149.2#53: host unreachable
+;; communications error to 192.168.149.2#53: host unreachable
+"onetrust-domain-verification=de01ed21f2fa4d8781cbc3ffb89cf4ef"
+"docusign=05958488-4752-4ef2-95eb-aa7ba8a3bd0e"
+"globalsign-smime-dv=CDYX+XFHUw2wml6/Gb8+59BsH31KzUr6c1l2BPvqKX8="
+"google-site-verification=4ibFUgB-wXLQ_S7vsXVomSTVamuOXBiVAzpR5IZ87D0"
+"docusign=1b0a6754-49b1-4db5-8540-d2c12664b289"
+"facebook-domain-verification=22rm551cu4k0ab0bxsw536tlds4h95"
+"MS=E4A68B9AB2BB9670BCE15412F62916164C0B20BB"
+"cisco-ci-domain-verification=479146de172eb01ddee38b1a455ab9e8bb51542ddd7f1fa298557dfa7b22d963"
+"apple-domain-verification=30afIBcvSuDV2PLX"
+"google-site-verification=wD8N7i1JTNTkezJ49swvWW48f8_9xveREV4oB-0Hf5o"
+"google-site-verification=TV9-DBe4R80X4v0M4U_bd_J9cpOJM0nikft0jAgjmsQ"
+"v=spf1 include:_spf.google.com ~all"
+[*] Checking server software version...
+Server Info: server: gws
+[*] Checking for exposed directories and files...
+[*] Checking if directory listing is enabled...
+✅ No directory listing detected.
+[*] Searching for JavaScript files...
+[*] Extracting email addresses from the page...
+[*] Checking for common vulnerabilities...
